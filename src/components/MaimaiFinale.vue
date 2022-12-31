f1ab7423d3273a8bf33708b3c6f5ec7c86678048
Kyzoid <kyzoid@gmail.com>
2022-12-31 15:54:07 +0100

    feat(maimaiFinale): add title, update style

--- src/components/MaimaiFinale.vue
+++ src/components/MaimaiFinale.vue
@@ -1,8 +1,10 @@
 <script setup>
 import maiPageTitleBg from '/maimai_finale/maiPageTitleBg.png';
-import avatar from '/maimai_finale/avatar.jpg';
+import avatar from '/maimai_finale/avatar.png';
 import star from '/maimai_finale/star.png';
 import starSolo from '/maimai_finale/star_solo.png';
+// import titleSilver from '/maimai_finale/title_silver.svg';
+import titleBlank from '/maimai_finale/title_blank.svg';
 </script>
 
 <template>
@@ -21,19 +23,31 @@ import starSolo from '/maimai_finale/star_solo.png';
           <img class="absolute" :src="maiPageTitleBg" alt="Maimai page title background" />
           <h1 class="absolute text-3xl font-bold">プロフィール</h1>
         </div>
+
         <div class="card p-1.5 rounded-lg mt-4">
-          <div class="card-inner flex items-start p-4 rounded-lg">
-            <div class="avatar rounded h-32 w-32"><img :src="avatar" alt="Profile picture"></div>
-            <div class="w-64 ml-4">
-              <h2 class="text-2xl uppercase name mb-1">Kyzoid</h2>
-              <div class="description mb-2 w-full px-3 py-0.5 text-black rounded-full text-xs">debut</div>
-              <div class="separator"></div>
-              <div class="flex">
-                <div class="mt-3 rating py-1 px-6 flex items-center justify-center rounded relative">
+          <div class="card-inner flex p-4 rounded-lg h-full">
+            <div class="avatar rounded"><img :src="avatar" alt="Profile picture"></div>
+            
+            <div class="w-64 ml-4 flex flex-col justify-between">
+              <div class="mt-0.5 ml-0.5 flex justify-between">
+                <div class="rating rating-blue pr-2 pl-5 flex items-center justify-center rounded relative">
                   <img class="absolute img-top" :src="starSolo" alt="Star top">
                   <img class="absolute img-bottom" :src="star" alt="Star bottom">
-                  <span class="uppercase text-xs mr-1.5">Rating</span>
-                  <span class="text-lg">11.97</span>
+                  <span class="label uppercase mr-1">Rating</span>
+                  <span>2.78</span>
+                </div>
+                <div class="level flex flex-col items-end font-bold">
+                  <span class="label">TOTAL Lv.</span>
+                  <span>12</span>
+                </div>
+              </div>
+
+              <div class="flex flex-col mb-1">
+                <h2 class="text-xl uppercase name mb-1">Kyzoid</h2>
+                <div class="relative description">
+                  <img class="absolute" :src="titleBlank" alt="Title silver" />
+                  <!-- <span class="absolute ml-4 mt-0.5">ソルトにガチ恋</span> -->
+                  <span class="absolute ml-4 mt-1">debut</span>
                 </div>
               </div>
             </div>
@@ -55,7 +69,15 @@ import starSolo from '/maimai_finale/star_solo.png';
 }
 
 .name {
-  letter-spacing: 0.05em;
+  letter-spacing: 0.1em;
+  text-shadow: black 1px 1px 0, black -1px -1px 0,
+		black -1px 1px 0, black 1px -1px 0,
+		black 0px 1px 0, black 0 -1px 0,
+		black -1px 0 0, black 1px 0 0;
+}
+
+.level span {
+  font-size: 10px;
   text-shadow: black 1px 1px 0, black -1px -1px 0,
 		black -1px 1px 0, black 1px -1px 0,
 		black 0px 1px 0, black 0 -1px 0,
@@ -63,53 +85,85 @@ import starSolo from '/maimai_finale/star_solo.png';
 }
 
 .description {
-  background-color: #FFFBF0;
-  outline: 2px solid #0A1557;
-  /* border: 1px solid #0A1557; */
+  height: 28px;
+  /* filter: drop-shadow(0 0 1px lightgrey); */
 }
 
-.rating {
+.avatar {
+  height: 112px;
+  width: 112px;
+}
+
+.description > span {
+  color: black;
+  /* text-shadow: black 0px 1px 0, black 1px 1px 0, black -1px 0px 0; silver */
+}
+
+.rating-red {
   background-color: #DC3D3F;
   outline: 2px solid #9C2222;
-  box-shadow: 0 0 2px 4px rgba(255, 180, 180, 0.7);
   border: 2px dotted rgba(255, 191, 191, 0.5);
 }
 
+.rating-blue {
+  background-color: #7CC1D3;
+  outline: 2px solid #56B4D3;
+  border: 2px dotted rgba(191, 251, 255, 0.5);
+}
+
+.rating .label {
+  font-size: 10px;
+}
+
 .rating > .img-bottom {
-  left: -0.7rem;
-  top: 0.15rem;
-  transform: rotate(-145deg) scale(0.7);
+  left: -0.9rem;
+  top: -0.25rem;
+  transform: rotate(-145deg) scale(0.6);
   opacity: 0.4; 
 }
 
+.rating-red > .img-bottom {
+  filter: hue-rotate(0deg);
+}
+
+.rating-blue > .img-bottom {
+  filter: hue-rotate(220deg);
+}
+
 .rating > .img-top {
-  left: -0.7rem;
-  top: -0.3rem;
-  transform: rotate(90deg) scale(0.7);
-  opacity: 0.4; 
+  left: -0.9rem;
+  top: -0.55rem;
+  transform: rotate(90deg) scale(0.6);
+  opacity: 0.4;
+}
+
+.rating-red > .img-top {
+  filter: hue-rotate(0deg);
+}
+
+.rating-blue > .img-top {
+  filter: hue-rotate(220deg);
 }
 
 .rating > span {
   font-weight: 600;
   letter-spacing: 0.1em;
-  text-shadow: 1px 1px 1px black, 2px 2px 1px black, 0px 1px 1px black, 0px 0px 1px black;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7), 2px 2px 1px rgba(0, 0, 0, 0.7), 0px 1px 1px rgba(0, 0, 0, 0.7), 0px 0px 1px rgba(0, 0, 0, 0.7);
 }
 
 .separator {
   border-bottom: 2px dotted #fff;
 }
 
-.avatar {
-  border: 3px solid #9bbbf7;
-}
-
 .card {
-  background-color: #2d3f7e;
-  box-shadow: inset 0 0 0 2px #43c3cb, 0 0 8px #000;
+  background-color: rgba(45, 63, 126, 0.8);
+  /* background: linear-gradient(0deg, rgba(255,140,253,0.1) 0%, rgba(140,140,255,0.1) 25%, rgba(140,255,215,0.1) 50%, rgba(255,255,140,0.1) 75%, rgba(246,140,255,0.09567577030812324) 100%); */
+  /* background: linear-gradient(0deg, rgba(255,180,254,0.2) 0%, rgba(180,180,255,0.2) 25%, rgba(255,255,180,0.2) 50%, rgba(246,140,255,0.2) 75%); */
+  box-shadow: inset 0 0 0 2px #43C3CB, 0 0 8px #000;
 }
 
 .card-inner {
-  border: 1px dashed #425da9;
+  border: 1px dashed #425DA9;
 }
 
 .title {
