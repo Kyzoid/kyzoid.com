dd3faf103f4048a9afbf6b344634e4f06ae8f257
Kyzoid <kyzoid@gmail.com>
2023-01-03 22:06:59 +0100

    feat(osu): add stats and background

--- src/components/osu/OsuBackground.vue
+++ src/components/osu/OsuBackground.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="bg-main relative h-full w-full">
+    <div class="triangles"></div>
   </div>
 </template>
 
@@ -10,7 +11,28 @@ defineProps({
 </script>
 
 <style scoped>
-.bg-main {
+.bg-main { 
   background-color: #1C1719;
+}
+
+.triangles {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url("/osu/triangles.png");
+  background-repeat: repeat-y;
+  background-size: 100%;
+  opacity: 0.3;
+  animation: backgroundMotion 100s linear infinite;
+}
+
+@keyframes backgroundMotion {
+  from {
+    background-position-y: -1200px;
+  }
+
+  to {
+    background-position-y: 1200px;
+  }
 }
 </style>
--- src/components/osu/OsuCard.vue
+++ src/components/osu/OsuCard.vue
@@ -10,28 +10,42 @@
         <li class="menu" :class="[ mode === 2 ? 'menu-active' : '']" @click="mode = 2">osu!mania</li>
       </ul>
     </div>
-    <div class="profile w-96 rounded-b flex p-4">
+    <div class="profile rounded-b flex items-start p-4">
       <img class="rounded-3xl" :src="avatar" alt="Avatar" width="112" />
       <div class="flex flex-col justify-between ml-4">
-        <div>
-          <div class="flex items-center">
-            <h2 class="text-lg font-medium">Kyzoid</h2>
-            <div class="mx-2 divider"></div>
-            <div class="pp">8093<span class="text-xs text-white">pp</span></div>
-          </div>
-          <div class="flex items-center">
+        <div class="flex items-center">
+          <h2 class="text-lg font-medium flex items-center">
             <img :src="flag" width="22" alt="France flag" />
-            <span class="ml-1 text-sm">France</span>
+            <span class="ml-2">Kyzoid</span>
+          </h2>
+          <div v-if="currentMode.pp" class="mx-2 divider"></div>
+          <div v-if="currentMode.pp" class="pp text-sm">
+            {{ currentMode.pp }}<span class="text-xs text-white">pp</span>
           </div>
         </div>
-        <div class="flex items-center">
-          <div class="flex flex-col mr-4">
-            <span class="text-xs">Classement Global</span>
-            <span class="text-xl rank">#2 924</span>
+        <div class="stats px-2 py-2 mt-2 rounded-lg">
+          <div class="px-2 flex items-center justify-between">
+            <div class="flex flex-col mr-4">
+              <span class="text-xs">Classement Global</span>
+              <span v-if="currentMode.globalRank" class="text-lg rank -mt-1">#{{ currentMode.globalRank }}</span>
+              <span v-else class="text-xl rank font-bold -mt-1">-</span>
+            </div>
+            <div class="flex flex-col">
+              <span class="text-xs">Classement Pays</span>
+              <span v-if="currentMode.countryRank" class="text-lg rank -mt-1">#{{ currentMode.countryRank }}</span>
+              <span v-else class="text-xl rank font-bold -mt-1">-</span>
+            </div>
           </div>
-          <div class="flex flex-col">
-            <span class="text-xs">Classement Pays</span>
-            <span class="text-xl rank">#41</span>
+          <div class="stats-divider my-1"></div>
+          <div class="text-xs px-2 py-0.5">
+            <div class="flex items-center justify-between">
+              <span>Accuracy</span>
+              <span>{{ currentMode.accuracy }}</span>
+            </div>
+            <div class="flex items-center justify-between">
+              <span>Play count</span>
+              <span>{{ currentMode.playCount }}</span>
+            </div>
           </div>
         </div>
       </div>
@@ -40,16 +54,39 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import avatar from '/osu/avatar.jpg';
 import flag from '/osu/fr.svg';
 
-const mode = ref(0);
+const mode = ref(2);
+const data = {
+  0 : { pp: null, globalRank: null, countryRank: null, accuracy: '98,97%', playCount: '25 880' },
+  1 : { pp: null, globalRank: null, countryRank: null, accuracy: '98,23%', playCount: '2 945' },
+  2 : { pp: 8093, globalRank: '2 924', countryRank: 41, accuracy: '94,69%', playCount: '20 889' },
+}
+
+const currentMode = computed(() => {
+  return data[mode.value];
+});
 </script>
 
 <style scoped>
+* {
+  font-family: 'Torus';
+}
+
 .wrapper {
   box-shadow: 0 1px 3px rgb(0 0 0 / 25%);
+}
+
+.stats {
+  background-color: #2A2226;
+}
+
+.stats-divider {
+  border-top: 2px solid rgb(28, 23, 25);
+  height: 1px;
+  width: 100%;
 }
 
 .infos {
@@ -88,6 +125,7 @@ const mode = ref(0);
 
 .profile {
   background-color: #382E32;
+  width: max-content;
 }
 
 .profile-header {
