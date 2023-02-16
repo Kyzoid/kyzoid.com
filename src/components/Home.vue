b821f3a063faf8a36a84e8b12f6d219b851922a4
Kyzoid <kyzoid@gmail.com>
2023-02-16 22:05:58 +0100

    refactor(monitoring): use dynamic components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div v-if="currentComponent" class="flex justify-center select-none items-center h-full overflow-hidden">
     <div class="carousel-wrapper masked-overflow absolute bottom-10 z-20">
-      <Swiper ref="swiper" :breakpoints="breakpoints" :centeredSlides="true" :loop="false" :initial-slide="currentComponent.id"
-        :simulateTouch="true" :slideToClickedSlide="true" @swiper="onSwiper" @slideChange="onSlideChange"
-        watch-slides-progress>
+      <Swiper ref="swiper" :breakpoints="breakpoints" :centeredSlides="true" :loop="false"
+        :initial-slide="currentComponent.id" :simulateTouch="true" :slideToClickedSlide="true"
+        @slideChange="onSlideChange" watch-slides-progress>
         <SwiperSlide class="slide" data-name="sdvx"><img :src="sdvx" alt="SDVX Logo" /></SwiperSlide>
-        <SwiperSlide class="slide" data-name="maimaiDeluxe"><img :src="maimaiDeluxe" alt="Maimai Deluxe Logo" /></SwiperSlide>
-        <!-- <SwiperSlide class="slide" data-name="chunithm"><img :src="chunithm" alt="Chunithm Logo" /></SwiperSlide> -->
+        <SwiperSlide class="slide" data-name="maimaiDeluxe"><img :src="maimaiDeluxe" alt="Maimai Deluxe Logo" />
+        </SwiperSlide>
         <SwiperSlide class="slide" data-name="ez2on"><img :src="ez2on" alt="Ez2On Logo" /></SwiperSlide>
-        <!-- <SwiperSlide class="slide" data-name="popn"><img :src="popn" alt="Pop'N Music Logo" /></SwiperSlide> -->
         <SwiperSlide class="slide" data-name="jubeat"><img :src="jubeat" alt="Jubeat Logo" /></SwiperSlide>
         <SwiperSlide class="slide" data-name="beatsaber"><img :src="beatsaber" alt="Beatsaber Logo" /></SwiperSlide>
-        <!-- <SwiperSlide class="slide" data-name="iidx"><img :src="iidx" alt="IIDX Logo" /></SwiperSlide> -->
-        <!-- <SwiperSlide class="slide" data-name="maimaiFinale"><img :src="maimaiFinale" alt="Maimai Finale Logo" /></SwiperSlide> -->
         <SwiperSlide class="slide" data-name="osu"><img :src="osu" alt="osu! Logo" /></SwiperSlide>
         <SwiperSlide class="slide" data-name="quaver"><img :src="quaver" alt="Quaver Logo" /></SwiperSlide>
         <SwiperSlide class="slide" data-name="etterna"><img :src="etterna" alt="Etterna Logo" /></SwiperSlide>
@@ -29,55 +26,46 @@
         <component class="absolute" :is="currentComponent.background" />
       </Transition>
     </div>
-  </div>
+</div>
 </template>
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import { ref, computed, watch, onMounted } from 'vue';
-import router from '../router';
+import { useRouter } from 'vue-router';
 
 // cards
 import BeatSaberCard from './beatsaber/BeatSaberCard.vue';
-import ChunithmCard from './chunithm/ChunithmCard.vue';
 import EtternaCard from './etterna/EtternaCard.vue';
 import Ez2onCard from './ez2on/Ez2onCard.vue';
 import MaimaiDeluxeCard from './maimai/MaimaiDeluxeCard.vue';
-import MaimaiFinaleCard from './maimai/MaimaiFinaleCard.vue';
 import OsuCard from './osu/OsuCard.vue';
 import JubeatCard from './jubeat/JubeatCard.vue';
-import PopnCard from './popn/PopnCard.vue';
 import QuaverCard from './quaver/QuaverCard.vue';
 import SDVXCard from './sdvx/SDVXCard.vue';
 
 // backgrounds
 import BeatSaberBackground from './beatsaber/BeatSaberBackground.vue';
-import ChunithmBackground from './chunithm/ChunithmBackground.vue';
 import EtternaBackground from './etterna/EtternaBackground.vue';
 import Ez2onBackground from './ez2on/Ez2onBackground.vue';
 import MaimaiDeluxeBackground from './maimai/MaimaiDeluxeBackground.vue';
-import MaimaiFinaleBackground from './maimai/MaimaiFinaleBackground.vue';
 import OsuBackground from './osu/OsuBackground.vue';
 import JubeatBackground from './jubeat/JubeatBackground.vue';
-import PopnBackground from './popn/PopnBackground.vue';
 import QuaverBackground from './quaver/QuaverBackground.vue';
 import SDVXBackground from './sdvx/SDVXBackground.vue';
 
 // logo
 import beatsaber from '/beatsaber/logo.svg';
-// import chunithm from '/chunithm/logo.png';
 import etterna from '/etterna/logo.png';
 import ez2on from '/ez2on/logo.png';
 import jubeat from '/jubeat/logo_sp.png';
 import maimaiDeluxe from '/maimai/logo.png';
-// import maimaiFinale from '/maimai_finale/logo.png';
 import osu from '/osu/logo.png';
-import popn from '/popn/logo.webp';
 import quaver from '/quaver/logo.png';
 import sdvx from '/sdvx/logo.png';
-import iidx from "/iidx/logo.png";
 
+const router = useRouter();
 const activeGame = ref(null);
 const swiper = ref(null);
 const slideTransition = ref("slide-left");
@@ -85,16 +73,12 @@ const slideTransition = ref("slide-left");
 const components = [
   { "id": 0, "name": "sdvx", "card": SDVXCard, "background": SDVXBackground },
   { "id": 1, "name": "maimaiDeluxe", "card": MaimaiDeluxeCard, "background": MaimaiDeluxeBackground },
-  // { "id": 0, "name": "chunithm", "card": ChunithmCard, "background": ChunithmBackground },
   { "id": 2, "name": "ez2on", "card": Ez2onCard, "background": Ez2onBackground },
-  // { "id": 3, "name": "popn", "card": PopnCard, "background": PopnBackground },
   { "id": 3, "name": "jubeat", "card": JubeatCard, "background": JubeatBackground },
   { "id": 4, "name": "beatsaber", "card": BeatSaberCard, "background": BeatSaberBackground },
-  // { "id": 0, "name": "maimaiFinale", "card": MaimaiFinaleCard, "background": MaimaiFinaleBackground },
   { "id": 5, "name": "osu", "card": OsuCard, "background": OsuBackground },
   { "id": 6, "name": "quaver", "card": QuaverCard, "background": QuaverBackground },
-  { "id": 7, "name": "etterna", "card": EtternaCard, "background": EtternaBackground },
-  // { "id": 0, "name": "iidx", "card": IIDXCard, "background": IIDXBackground },
+  { "id": 7, "name": "etterna", "card": EtternaCard, "background": EtternaBackground }
 ];
 
 onMounted(() => {
@@ -141,9 +125,6 @@ const breakpoints = {
     slidesPerView: 5,
     spaceBetween: 50
   }
-}
-
-const onSwiper = (swiper) => {
 }
 
 const onSlideChange = ({ activeIndex, previousIndex, slides }) => {
@@ -218,6 +199,8 @@ const onSlideChange = ({ activeIndex, previousIndex, slides }) => {
 .fade-leave-from {
   opacity: 1;
 }
+
+// ===============================================
 
 .carousel-wrapper {
   width: 100%;
--- src/components/Monitoring.vue
+++ src/components/Monitoring.vue
@@ -1,52 +1,76 @@
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-0">
-    <div class="relative h-80 overflow-hidden">
-      <SDVX mode="compact" />
-    </div>
+    <div v-for="component in components" class="relative h-80 overflow-hidden">
+      <component :is="component.background" />
 
-    <div class="relative h-80">
-      <MaimaiDeluxe mode="compact" class="relative h-full w-full" />
-    </div>
+      <div class="logo">
+        <img :src="component.logo" :alt="`${component.name} logo`" />
+      </div>
 
-    <div class="relative h-80">
-      <Jubeat mode="compact" />
-    </div>
-
-    <div class="relative h-80">
-      <Ez2on mode="compact" />
-    </div>
-
-    <div class="relative h-80">
-      <BeatSaber mode="compact" />
-    </div>
-
-    <div class="relative h-80">
-      <Osu mode="compact" />
-    </div>
-
-    <div class="relative h-80">
-      <Quaver mode="compact" />
-    </div>
-
-    <div class="relative h-80">
-      <Etterna mode="compact" />
+      <component :class="component.class" :is="component.card" />
     </div>
   </div>
 </template>
 
 <script setup>
-import MaimaiDeluxe from './MaimaiDeluxe.vue';
-import MaimaiFinale from './MaimaiFinale.vue';
-import SDVX from './SDVX.vue';
-import Popn from './Popn.vue';
-import BeatSaber from './BeatSaber.vue';
-import Ez2on from './Ez2on.vue';
-import Osu from './Osu.vue';
-import Quaver from './Quaver.vue';
-import Etterna from './Etterna.vue';
-import Jubeat from './Jubeat.vue';
-import Chunithm from './Chunithm.vue';
+// cards
+import BeatSaberCard from './beatsaber/BeatSaberCard.vue';
+import EtternaCard from './etterna/EtternaCard.vue';
+import Ez2onCard from './ez2on/Ez2onCard.vue';
+import MaimaiDeluxeCard from './maimai/MaimaiDeluxeCard.vue';
+import OsuCard from './osu/OsuCard.vue';
+import JubeatCard from './jubeat/JubeatCard.vue';
+import QuaverCard from './quaver/QuaverCard.vue';
+import SDVXCard from './sdvx/SDVXCard.vue';
+
+// backgrounds
+import BeatSaberBackground from './beatsaber/BeatSaberBackground.vue';
+import EtternaBackground from './etterna/EtternaBackground.vue';
+import Ez2onBackground from './ez2on/Ez2onBackground.vue';
+import MaimaiDeluxeBackground from './maimai/MaimaiDeluxeBackground.vue';
+import OsuBackground from './osu/OsuBackground.vue';
+import JubeatBackground from './jubeat/JubeatBackground.vue';
+import QuaverBackground from './quaver/QuaverBackground.vue';
+import SDVXBackground from './sdvx/SDVXBackground.vue';
+
+// logos
+import BeatsaberLogo from '/beatsaber/logo.svg';
+import EtternaLogo from '/etterna/logo.png';
+import SDVXLogo from '/sdvx/logo.png';
+import QuaverLogo from '/quaver/logo.png';
+import OsuLogo from '/osu/logo.png';
+import MaimaiDeluxeLogo from '/maimai/kv_logo.png';
+import JubeatLogo from '/jubeat/logo_sp.png';
+import Ez2onLogo from '/ez2on/logo.png';
+
+const components = [
+  { "id": "sdvx", "name": "SDVX", "card": SDVXCard, "background": SDVXBackground, "logo": SDVXLogo, "class": "card" },
+  { "id": "maimaiDeluxe", "name": "Maimai Deluxe", "card": MaimaiDeluxeCard, "background": MaimaiDeluxeBackground, "logo": MaimaiDeluxeLogo, "class": "card max-content" },
+  { "id": "ez2on", "name": "EZ2ON", "card": Ez2onCard, "background": Ez2onBackground, "logo": Ez2onLogo, "class": "card max-content" },
+  { "id": "jubeat", "name": "Jubeat", "card": JubeatCard, "background": JubeatBackground, "logo": JubeatLogo, "class": "card max-content" },
+  { "id": "beatsaber", "name": "Beatsaber", "card": BeatSaberCard, "background": BeatSaberBackground, "logo": BeatsaberLogo, "class": "card max-content" },
+  { "id": "osu", "name": "osu!", "card": OsuCard, "background": OsuBackground, "logo": OsuLogo, "class": "card max-content" },
+  { "id": "quaver", "name": "Quaver", "card": QuaverCard, "background": QuaverBackground, "logo": QuaverLogo, "class": "card max-content" },
+  { "id": "etterna", "name": "Etterna", "card": EtternaCard, "background": EtternaBackground, "logo": EtternaLogo, "class": "card max-content" },
+];
 </script>
 
 <style scoped>
+.logo-beatsaber {
+  position: absolute;
+  top: .5rem;
+  left: .5rem;
+  width: 50px;
+}
+
+.card {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.max-content {
+  width: max-content;
+}
 </style>
